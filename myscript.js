a769d69ec46e2840e72ec56a082568dd8e3ce2f7
meilanzheng <meilan.zheng@baruchmail.cuny.edu>
2017-03-15 16:08:51 -0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,118 @@
+
+  function changetext(){
+	  document.getElementById("p1").innerHTML="This will work!"; 
+  }
+  
+  function p1Over() {
+      document.getElementById("p1").innerHTML = "It is a great tool for Data Vidualization, I like to use it.";
+  }
+  
+  function p1Out() {
+      document.getElementById("p1").innerHTML = "Tableau";
+  }
+
+
+  function p2Over() {
+	  document.getElementById("p2").innerHTML = "This website is built by myself using HTML,CSS, and JavaScript";
+  }
+
+  function p2Out() {
+	  document.getElementById("p2").innerHTML = "HTML/CSS/JavaScript";
+  }
+  
+  
+  function p3Over() {
+	  document.getElementById("p3").innerHTML = "Check out my profile image. I enjoy photo manipulation";
+  }
+
+  function p3Out() {
+	  document.getElementById("p3").innerHTML = "Photoshop";
+  }
+  
+  function p4Over() {
+	  document.getElementById("p4").innerHTML = "I'm taking Python class now. I'm doing well";
+  }
+
+  function p4Out() {
+	  document.getElementById("p4").innerHTML = "Python";
+  }
+  
+  function p5Over() {
+	  document.getElementById("p5").innerHTML = "Created program for course projects with superial result";
+  }
+
+  function p5Out() {
+	  document.getElementById("p5").innerHTML = "C++, JAVA";
+  }
+  
+  function p6Over() {
+	  document.getElementById("p6").innerHTML = "I'm a Certified MS Office Specialist";
+  }
+
+  function p6Out() {
+	  document.getElementById("p6").innerHTML = "Excel";
+  }
+  
+  function p8Over() {
+	  document.getElementById("p8").innerHTML = "I can make aggregare queries, subqueries, make reports, creat view, and flatten data in Oracle SQL Developer";
+  }
+
+  function p8Out() {
+	  document.getElementById("p8").innerHTML = "SQL";
+  }
+  
+  function p7Over() {
+	  document.getElementById("p7").innerHTML = "Firm knowledge of Financial Accounting and Managerial Accounting. Experienced working as a Audit Analyst, NYC Finance department intern, accounting tutor, and Bank Teller. Minor in Corporate Communicate Communication.";
+  }
+
+  function p7Out() {
+	  document.getElementById("p7").innerHTML = "Financial Accounting <br> Managerial Accounting <br> Corporate Communication";
+  }
+  
+  
+  
+  
+  // function flickrbutton()
+//  {
+// 	var myvalue = window.prompt("What is the search term");
+// 	alert(myvalue);
+//
+// 	searchFlicker(myvalue);
+//
+//  }
+//
+//  function searchFlicker(thevalue) {
+//
+//  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+//      $.getJSON( flickerAPI, {  // The "$" simply refers to jQuery. This calls the getJSON function that is found inside jquery-3.1.1.min.js
+//        tags: "Baruch College", //Some filter information in the format set by Flickr, who owns the JSON
+//        tagmode: "any",
+//        format: "json"
+//      })
+//
+//
+//   .done(function(data)
+//  	{ //Here, a an unnamed function is created made into the event handler for the "done" event... in other words, this is the code that will manifest itself when the getJSON is done.
+//          $.each( data.items, function( i, item ) {
+//            $("<img>").attr( "src", item.media.m ).appendTo( "#images" );
+//            if ( i === 5 ) {
+//              return false;
+//          }
+//        });
+//      });
+//  }
+//
+ 
+ 
+ 
+ 
+ 
+
+  
+  
+  
+  
+  
+  
+  
+  
